ee48a2f8ccda956a3f837ede1b2cf8d01e250682
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2025-11-17 22:08:19 -0300

    PIXPAY

--- api/create-pix-payment.js
+++ api/create-pix-payment.js
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
           first_name: name,
           identification: {
             type: 'CPF',
-            number: '12356566678', // CPF de teste fornecido pelo usuário
+            number: '12356566678', // CPF de teste padrão
           },
         },
         // notification_url: `${process.env.APP_URL}/api/payment-webhook`, // This will be enabled in production with a public URL
